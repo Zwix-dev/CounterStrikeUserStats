89d3672f57448b3eeb7673e6017d6a614e269ba8
Arthur <arthur@MacBook-Pro-de-Arthur.local>
2025-03-20 12:30:05 +0100

    add text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import InputSearch from "@/components/inputSearch";
 export default async function Home() {
 
   return (
-    <main className="h-screen bg-gray-50">
-      <section className="bg-[url('/cs2-bg.png')] bg-cover bg-center h-full ">
-        <div className="text-6xl font-bold flex flex-row justify-center gap-6 pt-52">
+    <main className="h-screen bg-gray-50 overflow-hidden">
+      <section className="bg-[url('/cs2-bg.png')] bg-cover bg-center h-screen">
+        <div className="text-6xl font-bold md:flex flex-row justify-center md:gap-6 pt-22 md:pt-56 text-center">
           <h1 className=" text-white">TROUVE</h1>
           <h1 className="text-purple-300 "> LES </h1>
           <h1 className="text-purple-500"> CHEATERS </h1>
